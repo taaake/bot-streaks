5317b0ac8e9e51d63c310de517d85ca0a37058a1
TAKe <94477820+taaake@users.noreply.github.com>
2024-11-26 22:34:28 +0100

    Smeh j'ai oublier le changement de db

--- src/managers/user/FlammesManager.ts
+++ src/managers/user/FlammesManager.ts
@@ -1,39 +1,46 @@
-import { QuickDB } from "quick.db";
-const db = new QuickDB();
+import { Flames } from "../../db/models/Flammes";
+import { Duo } from "../../db/models/Duo";
 
 export const checkFlammes = async (client: any) => {
-  const allDuos = await db.all();
+  const allDuos = await Duo.findAll();
+  for (const duo of allDuos) {
+    const { userId, partnerId } = duo;
+    let flameRecord = await Flames.findOne({
+      where: { userId, partnerId },
+    });
 
-  for (const record of allDuos) {
-    if (record.id.startsWith("flammes_")) {
-      const [_, userId, duoPartnerId] = record.id.split("_");
-      const lastImageTimestamp = await db.get(
-        `lastImage_${userId}_${duoPartnerId}`
-      );
+    if (!flameRecord) {
+      flameRecord = await Flames.create({
+        userId,
+        partnerId,
+        count: 0,
+        lastUpdate: new Date(0),
+      });
+    }
 
-      if (!lastImageTimestamp) {
-        await db.set(`flammes_${userId}_${duoPartnerId}`, 0);
-        continue;
-      }
-
-      const currentTime = Date.now();
-      const timeDifference = currentTime - lastImageTimestamp;
-
-      if (timeDifference > 24 * 60 * 60 * 1000) {
-        await db.set(`flammes_${userId}_${duoPartnerId}`, 0);
-        const user = client.users.get(userId);
-        const duoPartner = client.users.get(duoPartnerId);
-
-        if (user && duoPartner) {
-          user.createDM().then((dm: any) =>
-            dm.createMessage({
-              content: ` Vos flammes avec \${duoPartner.username}\ a été réinitialisé car aucune image n'a été envoyée dans les 24 dernières heures...`,
-            })
+    const currentTime = Date.now();
+    const lastUpdateTime = new Date(flameRecord.lastUpdate).getTime();
+    const timeDifference = currentTime - lastUpdateTime;
+    if (timeDifference > 24 * 60 * 60 * 1000) {
+      flameRecord.count = 0;
+      await flameRecord.save();
+      const user = client.users.get(userId);
+      const duoPartner = client.users.get(partnerId);
+      if (user && duoPartner) {
+        try {
+          const userDM = await user.createDM();
+          const partnerDM = await duoPartner.createDM();
+          await userDM.send(
+            `Vos flammes avec ${duoPartner.username} ont été réinitialisées car aucune image n'a été envoyée dans les 24 dernières heures...`
           );
-          duoPartner.createDM().then((dm: any) =>
-            dm.createMessage({
-              content: `Votre flammes avec \${user.username}\ a été réinitialisé car aucune image n'a été envoyée dans les 24 dernières heures...`,
-            })
+
+          await partnerDM.send(
+            `Vos flammes avec ${user.username} ont été réinitialisées car aucune image n'a été envoyée dans les 24 dernières heures...`
+          );
+        } catch (error) {
+          console.error(
+            `Erreur lors de l'envoi du message aux utilisateurs ${userId} et ${partnerId}: `,
+            error
           );
         }
       }
